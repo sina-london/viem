0b4df86c35c76b3c7550c05e97d92b98caa5e6c6
sina-london <sina@clica.tech>
2024-09-28 20:48:59 +0800

    Create visionTestnet.ts

---
+++ src/chains/definitions/visionTestnet.ts
@@ -0,0 +1,18 @@
+import { defineChain } from '../../utils/chain/defineChain.js'
+
+export const visioTestnet = /*#__PURE__*/ defineChain({
+  id: 666666,
+  name: 'Vision Testnet',
+  nativeCurrency: { name: 'VISION', symbol: 'VS', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://vpioneer.infragrid.v.network/ethereum/compatible'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Vision Scan',
+      url: 'https://visionscan.org/?chain=vpioneer',
+    },
+  },
+})
